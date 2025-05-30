925db69be6b0b4d95d77f1ae0ca34d632b8a2bf6
MobileExpert1 <appsdev096@gmail.com>
2025-05-30 14:22:41 +0530

    Added the Logo Added the First/Last Name with functionality. Updated the background colour of signup button

--- lib/SignupScreen.dart
+++ lib/SignupScreen.dart
@@ -13,114 +13,134 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController =
-      TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Create your account'),
-        surfaceTintColor: Colors.white,
+    return BlocProvider(
+      create: (_) => SignupCubit(),
+      child: Scaffold(
         backgroundColor: Colors.white,
-        centerTitle: true,
-        shape: Border(bottom: BorderSide(color: Colors.grey, width: 1)),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: BlocBuilder<SignupCubit, SignupState>(
-            builder: (context, state) {
-              return Column(
-                children: [
-                  SizedBox(height: 20),
-                  SvgPicture.asset('assets/Logo.svg'),
-                  SizedBox(height: 30),
-                  CustomTextField(
-                    label: 'Email',
-                    controller: emailController,
-                    errorText: state.emailError,
-                    onChanged: (val) =>
-                        context.read<SignupCubit>().emailChanged(val),
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextField(
-                    label: 'Password',
-                    controller: passwordController,
-                    errorText: state.passwordError,
-                    obscureText: true,
-                    onChanged: (val) =>
-                        context.read<SignupCubit>().passwordChanged(val),
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextField(
-                    label: 'Confirm Password',
-                    controller: confirmPasswordController,
-                    errorText: state.confirmPasswordError,
-                    obscureText: true,
-                    onChanged: (val) =>
-                        context.read<SignupCubit>().confirmPasswordChanged(val),
-                  ),
-                  SizedBox(height: 30),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 48,
-                    child: ElevatedButton(
-                      onPressed: state.isButtonEnabled
-                          ? () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomeScreen(),
-                                ),
-                              );
-                            }
-                          : null,
-                      child: Text('Start Subscription'),
+        appBar: AppBar(
+          title: Text('Create your account'),
+          surfaceTintColor: Colors.white,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          shape: Border(bottom: BorderSide(color: Colors.grey, width: 1)),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: BlocBuilder<SignupCubit, SignupState>(
+              builder: (context, state) {
+                return Column(
+                  children: [
+                    SizedBox(height: 20),
+                    SvgPicture.asset('assets/Logo.svg'),
+                    SizedBox(height: 30),
+                    CustomTextField(
+                      label: 'First Name',
+                      controller: firstNameController,
+                      errorText: state.firstNameError,
+                      onChanged: (val) =>
+                          context.read<SignupCubit>().firstNameChanged(val),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 48,
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
+                    SizedBox(height: 15),
+                    CustomTextField(
+                      label: 'Last Name',
+                      controller: lastNameController,
+                      errorText: state.lastNameError,
+                      onChanged: (val) =>
+                          context.read<SignupCubit>().lastNameChanged(val),
+                    ),
+                    SizedBox(height: 15),
+                    CustomTextField(
+                      label: 'Email',
+                      controller: emailController,
+                      errorText: state.emailError,
+                      onChanged: (val) =>
+                          context.read<SignupCubit>().emailChanged(val),
+                    ),
+                    SizedBox(height: 15),
+                    CustomTextField(
+                      label: 'Password',
+                      controller: passwordController,
+                      errorText: state.passwordError,
+                      obscureText: true,
+                      onChanged: (val) =>
+                          context.read<SignupCubit>().passwordChanged(val),
+                    ),
+                    SizedBox(height: 15),
+                    CustomTextField(
+                      label: 'Confirm Password',
+                      controller: confirmPasswordController,
+                      errorText: state.confirmPasswordError,
+                      obscureText: true,
+                      onChanged: (val) =>
+                          context.read<SignupCubit>().confirmPasswordChanged(val),
+                    ),
+                     SizedBox(height: 30),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 48,
+                      child: ElevatedButton(
+                        onPressed: state.isButtonEnabled
+                            ? () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                            ),
+                          );
+                        }
+                            : null,
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all(Colors.white),
+                          backgroundColor: MaterialStateProperty.all(
+                            state.isButtonEnabled
+                                ? Color.fromRGBO(63, 61, 81, 1.0)
+                                : Colors.grey,
+                          ),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
                         ),
-                        side: BorderSide(
-                          color: state.isButtonEnabled == true
-                              ? Colors.blue
-                              : Colors.grey,
-                        ),
+                        child: Text('Start Subscription'),
                       ),
-                      onPressed: state.isButtonEnabled
-                          ? () {
-                              print('Signup Successful!');
-                            }
-                          : null,
-                      child: Text('Free Trial 7 days'),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    child: Text(
-                      "Already a user? Log in",
-                      style: TextStyle(color: Colors.grey),
+                    SizedBox(height: 15),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginScreen()),
+                        );
+                      },
+                      child: Text(
+                        "Already a user? Log in",
+                        style: TextStyle(color: Colors.grey),
+                      ),
                     ),
-                  ),
-                ],
-              );
-            },
+                  ],
+                );
+              },
+            ),
           ),
         ),
       ),
--- lib/bloc/signup/signup_cubit.dart
+++ lib/bloc/signup/signup_cubit.dart
@@ -1,10 +1,19 @@
-// lib/bloc/signup/signup_cubit.dart
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'signup_state.dart';
 import '../../Constants/Validators.dart';
 
 class SignupCubit extends Cubit<SignupState> {
   SignupCubit() : super(SignupState());
+
+  void firstNameChanged(String firstName) {
+    final error = Validators().validateName(firstName);
+    _emitUpdatedState(firstName: firstName, firstNameError: error);
+  }
+
+  void lastNameChanged(String lastName) {
+    final error = Validators().validateName(lastName);
+    _emitUpdatedState(lastName: lastName, lastNameError: error);
+  }
 
   void emailChanged(String email) {
     final error = Validators().validateEmail(email);
@@ -17,50 +26,56 @@ class SignupCubit extends Cubit<SignupState> {
   }
 
   void confirmPasswordChanged(String confirmPassword) {
-    final error = Validators().validateConfirmPassword(
-      state.password,
-      confirmPassword,
-    );
-    _emitUpdatedState(
-      confirmPassword: confirmPassword,
-      confirmPasswordError: error,
-    );
+    final error = Validators().validateConfirmPassword(state.password, confirmPassword);
+    _emitUpdatedState(confirmPassword: confirmPassword, confirmPasswordError: error);
   }
 
   void _emitUpdatedState({
+    String? firstName,
+    String? lastName,
     String? email,
     String? password,
     String? confirmPassword,
+    String? firstNameError,
+    String? lastNameError,
     String? emailError,
     String? passwordError,
     String? confirmPasswordError,
   }) {
+    final newFirstName = firstName ?? state.firstName;
+    final newLastName = lastName ?? state.lastName;
     final newEmail = email ?? state.email;
     final newPassword = password ?? state.password;
     final newConfirmPassword = confirmPassword ?? state.confirmPassword;
 
-    // Revalidate each field if its error is not explicitly passed
-    final updatedEmailError =
-        emailError ?? Validators().validateEmail(newEmail);
-    final updatedPasswordError =
-        passwordError ?? Validators().validatePassword(newPassword);
-    final updatedConfirmPasswordError =
-        confirmPasswordError ??
+    final updatedFirstNameError = firstNameError ?? Validators().validateName(newFirstName);
+    final updatedLastNameError = lastNameError ?? Validators().validateName(newLastName);
+    final updatedEmailError = emailError ?? Validators().validateEmail(newEmail);
+    final updatedPasswordError = passwordError ?? Validators().validatePassword(newPassword);
+    final updatedConfirmPasswordError = confirmPasswordError ??
         Validators().validateConfirmPassword(newPassword, newConfirmPassword);
 
     final isValid =
-        updatedEmailError == null &&
-        updatedPasswordError == null &&
-        updatedConfirmPasswordError == null &&
-        newEmail.isNotEmpty &&
-        newPassword.isNotEmpty &&
-        newConfirmPassword.isNotEmpty;
+        updatedFirstNameError == null &&
+            updatedLastNameError == null &&
+            updatedEmailError == null &&
+            updatedPasswordError == null &&
+            updatedConfirmPasswordError == null &&
+            newFirstName.isNotEmpty &&
+            newLastName.isNotEmpty &&
+            newEmail.isNotEmpty &&
+            newPassword.isNotEmpty &&
+            newConfirmPassword.isNotEmpty;
 
     emit(
       state.copyWith(
+        firstName: newFirstName,
+        lastName: newLastName,
         email: newEmail,
         password: newPassword,
         confirmPassword: newConfirmPassword,
+        firstNameError: updatedFirstNameError,
+        lastNameError: updatedLastNameError,
         emailError: updatedEmailError,
         passwordError: updatedPasswordError,
         confirmPasswordError: updatedConfirmPasswordError,
