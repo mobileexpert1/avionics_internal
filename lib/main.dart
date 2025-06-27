d57c817115c628905084ed9d74b4c781f94ee8ee
MobileExpert1 <appsdev096@gmail.com>
2025-06-27 09:40:32 +0530

    Updated Code

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Avionica',
             theme: ThemeData(primarySwatch: Colors.blue),
-            home: AirbusScreen(),
+            home: SplashScreen(),
           );
         },
       ),
