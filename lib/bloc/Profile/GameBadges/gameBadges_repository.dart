d0ef699e2c8294c18424932a5f67595657aaebab
MobileExpert1 <appsdev096@gmail.com>
2025-11-14 17:40:58 +0530

    Updated Code

--- lib/bloc/Profile/GameBadges/gameBadges_repository.dart
+++ lib/bloc/Profile/GameBadges/gameBadges_repository.dart
@@ -27,75 +27,75 @@ class BadgesRepository {
     required int totalPoints,
   }) async {
     try {
-      // final uri = Uri.parse(
-      //   "${ApiBaseUrlConstant.baseUrl}"
-      //       "${ApiFunctionUrlGamesConstant.quiz}"
-      //       "${ApiGameBadges.quizBadges}",
-      // );
-      //
-      // final jsonData = await ApiService.get(url: uri) as Map<String, dynamic>;
-      // return BadgeResponse.fromJson(jsonData);
-      // Simulate network delay
-      await Future.delayed(const Duration(milliseconds: 800));
+      final uri = Uri.parse(
+        "${ApiBaseUrlConstant.baseUrl}"
+        "${ApiFunctionUrlGamesConstant.quiz}"
+        "${ApiGameBadges.quizBadges}",
+      );
 
-      // ✅ Local mock data
-      const jsonString = '''
-    {
-      "detail": "Quiz badges fetch successfully",
-      "total_earn_point": 0,
-      "data": [
-        {
-          "id": "138157f5-3821-4d52-8d59-c5fd110956f9",
-          "name": "Cloud Chaser",
-          "wins": 2,
-          "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560173%20(2).svg",
-          "is_earned": false,
-          "require_win": 2,
-          "total_win": 0
-        },
-        {
-          "id": "24e2adbc-d091-48e1-b8f8-be171e1bb1b7",
-          "name": "Noctilucent Explorer",
-          "wins": 2,
-          "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560175%20(1).png",
-          "is_earned": false,
-          "require_win": 2,
-          "total_win": 0
-        },
-        {
-          "id": "9e0599f2-b1a5-4d91-ac3e-def2a19e88e8",
-          "name": "Space Shuttle",
-          "wins": 2,
-          "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560177.png",
-          "is_earned": false,
-          "require_win": 2,
-          "total_win": 0
-        },
-        {
-          "id": "a2368f61-184d-478e-a636-653a050fe720",
-          "name": "Jetstream Voyager",
-          "wins": 2,
-          "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560174%20(1).png",
-          "is_earned": false,
-          "require_win": 2,
-          "total_win": 0
-        },
-        {
-          "id": "a71d58fa-a715-4e37-94ab-359d018e291a",
-          "name": "Aurora Sentinel",
-          "wins": 2,
-          "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560176%20(1).png",
-          "is_earned": false,
-          "require_win": 2,
-          "total_win": 0
-        }
-      ]
-    }
-    ''';
-
-      // ✅ Decode and return as BadgeResponse
-      final jsonData = jsonDecode(jsonString) as Map<String, dynamic>;
+      final jsonData = await ApiService.get(url: uri) as Map<String, dynamic>;
       return BadgeResponse.fromJson(jsonData);
+      //   // Simulate network delay
+      //   await Future.delayed(const Duration(milliseconds: 800));
+      //
+      //   // ✅ Local mock data
+      //   const jsonString = '''
+      // {
+      //   "detail": "Quiz badges fetch successfully",
+      //   "total_earn_point": 0,
+      //   "data": [
+      //     {
+      //       "id": "138157f5-3821-4d52-8d59-c5fd110956f9",
+      //       "name": "Cloud Chaser",
+      //       "wins": 2,
+      //       "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560173%20(2).svg",
+      //       "is_earned": false,
+      //       "require_win": 2,
+      //       "total_win": 0
+      //     },
+      //     {
+      //       "id": "24e2adbc-d091-48e1-b8f8-be171e1bb1b7",
+      //       "name": "Noctilucent Explorer",
+      //       "wins": 2,
+      //       "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560175%20(1).png",
+      //       "is_earned": false,
+      //       "require_win": 2,
+      //       "total_win": 0
+      //     },
+      //     {
+      //       "id": "9e0599f2-b1a5-4d91-ac3e-def2a19e88e8",
+      //       "name": "Space Shuttle",
+      //       "wins": 2,
+      //       "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560177.png",
+      //       "is_earned": false,
+      //       "require_win": 2,
+      //       "total_win": 0
+      //     },
+      //     {
+      //       "id": "a2368f61-184d-478e-a636-653a050fe720",
+      //       "name": "Jetstream Voyager",
+      //       "wins": 2,
+      //       "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560174%20(1).png",
+      //       "is_earned": false,
+      //       "require_win": 2,
+      //       "total_win": 0
+      //     },
+      //     {
+      //       "id": "a71d58fa-a715-4e37-94ab-359d018e291a",
+      //       "name": "Aurora Sentinel",
+      //       "wins": 2,
+      //       "icon": "https://avionica.csdevhub.com/s3/manufacturer/Group%201686560176%20(1).png",
+      //       "is_earned": false,
+      //       "require_win": 2,
+      //       "total_win": 0
+      //     }
+      //   ]
+      // }
+      // ''';
+      //
+      //   // ✅ Decode and return as BadgeResponse
+      //   final jsonData = jsonDecode(jsonString) as Map<String, dynamic>;
+      //   return BadgeResponse.fromJson(jsonData);
     } catch (e) {
       throw Exception("Failed to fetch quiz badges (local): $e");
     }
